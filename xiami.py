22bf5284edca7164632d7d14f79c641189b03219
Timothy Qiu <timothyqiu32@gmail.com>
2014-10-06 13:25:02 +0800

    [Ref]: Get rid of XML parsing

--- xiami.py
+++ xiami.py
@@ -7,9 +7,9 @@ import re
 import sys
 import urllib
 import urllib2
-import xml.etree.ElementTree as ET
 import json
 import httplib
+import HTMLParser
 from contextlib import closing
 from Cookie import SimpleCookie
 
@@ -29,10 +29,9 @@ except:
 VERSION = '0.3.1'
 
 URL_PATTERN_ID = 'http://www.xiami.com/song/playlist/id/%d'
-URL_PATTERN_SONG = '%s/object_name/default/object_id/0' % URL_PATTERN_ID
-URL_PATTERN_ALBUM = '%s/type/1' % URL_PATTERN_ID
-URL_PATTERN_PLAYLIST = '%s/type/3' % URL_PATTERN_ID
-URL_PATTERN_ALBUMFULL = 'http://www.xiami.com/app/android/album?id=%s'
+URL_PATTERN_SONG = URL_PATTERN_ID + '/object_name/default/object_id/0/cat/json'
+URL_PATTERN_ALBUM = URL_PATTERN_ID + '/type/1/cat/json'
+URL_PATTERN_PLAYLIST = URL_PATTERN_ID + '/type/3/cat/json'
 URL_PATTERN_VIP = 'http://www.xiami.com/song/gethqsong/sid/%s'
 
 HEADERS = {
@@ -107,28 +106,22 @@ def get_playlist_from_url(url):
 
 
 def parse_playlist(playlist):
-    try:
-        # Removes the XML namespace
-        playlist = re.sub(r'xmlns=\".*?\"', '', playlist)
-        # Replace chars like &#039;
-        matches = re.findall(r'(?<=&#)\d+(?=;)', playlist)
-        for match in matches:
-            playlist = playlist.replace('&#'+match+';', chr(int(match)))
-        playlist = playlist.replace('&quot;', '"').replace('&gt;', '>') \
-            .replace('&lt;', '<').replace('&amp;', '&')
-        xml = ET.fromstring(playlist)
-    except:
+    data = json.loads(playlist)
+
+    if not data['status']:
         return []
+
+    parser = HTMLParser.HTMLParser()
 
     return [
         {
-            key: track.find(key).text
+            key: parser.unescape(track[key])
             for key in [
                 'title', 'location', 'lyric', 'pic', 'artist', 'album_name',
                 'song_id', 'album_id'
             ]
         }
-        for track in xml.iter('track')
+        for track in data['data']['trackList']
     ]
 
 
